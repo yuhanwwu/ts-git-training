409566974450297037309bb89aeacc0e52945da1
Cameron McCormack <cameron.mccormack@softwire.com>
2022-06-18 09:54:23 +0100

    compute fibonnaci numbers in an array

--- src/fibonacci.ts
+++ src/fibonacci.ts
@@ -15,3 +15,8 @@ export const computeFibonacciNumber = (position: number): number => {
     }
     return j;
 };
+
+export const computeFibonacciArray = (start: number, endInclusive: number): number[] => {
+    const inputArray = [...Array(endInclusive - start + 1).keys()].map(i => i + start);
+    return inputArray.map(x => computeFibonacciNumber(x));
+}
