017a2749e81ff6a19db0ec6907796b8b68316289
Cameron McCormack <cameron.mccormack@softwire.com>
2022-06-18 09:08:00 +0100

    handle null input

--- src/fibonacci.ts
+++ src/fibonacci.ts
@@ -1,13 +1,18 @@
-export const computeFibonacciNumber = (position: number): number => {
+export const computeFibonacciNumber = (position: number | null): number => {
+    let notNullPosition = position;
+    if (notNullPosition === null) {
+        notNullPosition = 1;   
+    }
+
     let i = 1;
     let j = 1;
 
-    if (position <= 2) {
+    if (notNullPosition <= 2) {
         return 1;
     }
 
     let currentPosition = 2;
-    while (currentPosition < position) {
+    while (currentPosition < notNullPosition) {
         const temp = i;
         i = j;
         j += temp;
